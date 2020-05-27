b8563b1c1c6bc94445d1302aabdc57fca36ec456
Ariq Dhia Irfanudin <invanteris@gmail.com>
2020-05-27 07:54:50 +0700

    initial redux store

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,26 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-      <section className="contianer">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
-      <Footer />
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="contianer">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+        <Footer />
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
